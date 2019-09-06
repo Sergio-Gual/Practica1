cabe8f568e4765185d0235699a8b1a5c80c92736
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:19:06 -0500

    guardando primer cambio

---
+++ Clase1.Laboratorio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, suma;
+	
+    cout << "Ingrese un numeros" << endl;
+    cin>>x;
+    cout << "Ingrese un numeros" << endl;
+    cin>>y;
+    suma=x+y;
+    cout<<"La suma es"<<"---->"<<suma;
+    return 0;
+}
+	
